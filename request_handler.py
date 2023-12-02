230753c0e641e8e806ef1b5732bfbd7e850e350e
Nicholas Adams <njadams.722@gmail.com>
2023-12-02 14:27:12 -0600

    delete-resources

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_employee, create_customer
+from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_employee, create_customer, delete_animal, delete_customer, delete_employee, delete_location
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -80,8 +80,60 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
+    # def do_POST(self):
+    #     self._set_headers(201)
+    #     content_len = int(self.headers.get('content-length', 0))
+    #     post_body = self.rfile.read(content_len)
+
+    #     # Convert JSON string to a Python dictionary
+    #     post_body = json.loads(post_body)
+
+    #     # Parse the URL
+    #     (resource, id) = self.parse_url(self.path)
+
+    #     # Initialize new animal
+    #     new_animal = None
+
+    #     # Initialize new location
+    #     new_location = None
+
+    #     # Initialize new Employee
+    #     new_employee = None
+
+    #     # Initialize new Customer
+    #     new_customer = None
+
+    #     # Add a new animal to the list. Don't worry about
+    #     # the orange squiggle, you'll define the create_animal
+    #     # function next.
+    #     if resource == "animals":
+    #         new_animal = create_animal(post_body)
+        
+    #     # Add a new location to the list
+    #     if resource == "locations":
+    #         new_location = create_location(post_body)
+
+    #     # Add a new employee to the list 
+    #     if resource == "employees":
+    #         new_employee = create_employee(post_body)
+
+    #     # Add a new customer to the list
+    #     if resource == "customers":
+    #         new_customer = create_customer(post_body)
+
+    #     # Encode the new animal and send in response
+    #     self.wfile.write(json.dumps(new_animal).encode())
+    #     # Encode the new location and send in response
+    #     self.wfile.write(json.dumps(new_location).encode())
+    #     # Encode the new employee and send in response 
+    #     self.wfile.write(json.dumps(new_employee).encode())
+    #     # Encode the new customer and send in response
+    #     self.wfile.write(json.dumps(new_customer).encode())
+
     def do_POST(self):
+        # Combine response code and headers
         self._set_headers(201)
+
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
 
@@ -89,46 +141,31 @@ class HandleRequests(BaseHTTPRequestHandler):
         post_body = json.loads(post_body)
 
         # Parse the URL
-        (resource, id) = self.parse_url(self.path)
+        (resource, _) = self.parse_url(self.path)
 
-        # Initialize new animal
-        new_animal = None
+        # Define a mapping of resources to creation functions
+        resource_creation_map = {
+            "animals": create_animal,
+            "locations": create_location,
+            "employees": create_employee,
+            "customers": create_customer
+        }
 
-        # Initialize new location
-        new_location = None
+        # Initialize new resource
+        new_resource = None
 
-        # Initialize new Employee
-        new_employee = None
+        # Check if the resource is in the mapping
+        if resource in resource_creation_map:
+            # Call the corresponding creation function
+            new_resource = resource_creation_map[resource](post_body)
 
-        # Initialize new Customer
-        new_customer = None
+            # Encode the new resource and send in response
+            self.wfile.write(json.dumps(new_resource).encode())
+        else:
+            # Handle unsupported resource
+            self._set_headers(400)
+            self.wfile.write("Unsupported resource".encode())
 
-        # Add a new animal to the list. Don't worry about
-        # the orange squiggle, you'll define the create_animal
-        # function next.
-        if resource == "animals":
-            new_animal = create_animal(post_body)
-        
-        # Add a new location to the list
-        if resource == "locations":
-            new_location = create_location(post_body)
-
-        # Add a new employee to the list 
-        if resource == "employees":
-            new_employee = create_employee(post_body)
-
-        # Add a new customer to the list
-        if resource == "customers":
-            new_customer = create_customer(post_body)
-
-        # Encode the new animal and send in response
-        self.wfile.write(json.dumps(new_animal).encode())
-        # Encode the new location and send in response
-        self.wfile.write(json.dumps(new_location).encode())
-        # Encode the new employee and send in response 
-        self.wfile.write(json.dumps(new_employee).encode())
-        # Encode the new customer and send in response
-        self.wfile.write(json.dumps(new_customer).encode())
 
 
     def do_PUT(self):
@@ -159,6 +196,29 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers',
                          'X-Requested-With, Content-Type, Accept')
         self.end_headers()
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+        
+        if resource == "customers":
+            delete_customer(id)
+        
+        if resource == "employees":
+            delete_employee(id)
+
+        if resource == "locations":
+            delete_location(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode()) 
 
 
 # This function is not inside the class. It is the starting
